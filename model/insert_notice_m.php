a559995412c0006167dfafc5a42a3b001c3b907a
leebean <74585673+delight-HK3@users.noreply.github.com>
2022-04-21 10:27:45 +0900

    Update insert_notice_m.php

--- model/insert_notice_m.php
+++ model/insert_notice_m.php
@@ -1,10 +1,14 @@
 <?php
+if (!defined('BASEPATH')) exit('No direct script access allowed');
+
 class Insert_notice_m extends CI_Model {
-    public function insert($data,$no){
+    public function insert($data,$no){ 
+    	// insert함수가 아닌 update를 사용하는 이유는 기존에 빈 테이블 행을 만들었기 때문에 
+        // 빈테이블 번호와 같은 행을 view에서 가져온 값을 넣어 업데이트 합니다.
         $where=array("no"=>$no);
         return $this->db->update("innotice",$data,$where);
     }
-    public function make(){
+    public function make(){ // 빈 값을 가진 행을 하나 만들고 그중에 writer_no 가 0인 행을 선택합니다.
         $sql1 = "insert into innotice(name, writer_no, content) 
                     values (null,0,null)";
         $sql2 = "select * from innotice where writer_no like 0";
